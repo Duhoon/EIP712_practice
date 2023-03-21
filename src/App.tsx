6ad0288b0be9573d95a11f049c03310d89b7f724
Duhoon <eclip6@naver.com>
2023-03-21 17:14:56 +0900

    feat: Sign TypeData

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
@@ -6,6 +7,53 @@ function App() {
   const [to, setTo] = useState("");
   const [amount, setAmount] = useState("");
   const [signature, setSignature] = useState("");
+
+  const clickHandler = async ()=>{
+    const {ethereum} = window;
+    if (ethereum){
+      const account = await ethereum.request({method: 'eth_requestAccounts', params:[]});
+      setFrom(account[0]);
+
+      console.log(to, from, amount);
+
+      const typeData = {
+        types: {
+          EIP712Domain: [
+            {name:'name', type:'string'},
+            {name:'version', type:'string'},
+            {name:'chainId', type:'uint256'},
+            {name:'verifyingContract', type:'address'}
+          ],
+          Transfer: [
+            {name:'from', type:'address'},
+            {name:'to', type:'address'},
+            {name:'amount', type:'uint256'},
+          ]
+        },
+        primaryType: "Transfer",
+        domain: {
+          chainId: 5,
+          name: "PermitToken",
+          version: "1.0",
+          verifyingContract: '0x'
+        },
+        message: {
+          contents: "Approve To",
+          amount,
+          from,
+          to
+        }
+      }
+      
+      const result = await ethereum.request({
+        method: "eth_signTypedData_v4", 
+        params: [account[0], JSON.stringify(typeData)]
+      })
+
+      console.log(result);
+      setSignature(result);
+    }
+  }
 
   return (
     <div className="App">
@@ -27,7 +75,7 @@ function App() {
           />
         </div>
         <div className="button-group">
-          <button>Sign</button>
+          <button onClick={clickHandler}>Sign</button>
         </div>
         <div className="input-group">
           <label>Signature</label>
